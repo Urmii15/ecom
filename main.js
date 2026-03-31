98a7da8a7fb0284dfdeaa3ddbe55376b3e869e15
urmii15 <urmilasoni6363@gmail.com>
2026-03-31 15:30:07 +0530

    responsive

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
     cart.classList.remove('active');
-    user.classList.toggle('active');
+    user.classList.remove('active');
+    navbar.classList.remove('active');
+
 
 }
 
@@ -12,16 +14,35 @@ let cart = document.querySelector('.cart');
 document.querySelector('#cart-icon').onclick = () => {
     cart.classList.toggle('active');
     search.classList.remove('active');
-    user.classList.toggle('active');
+    user.classList.remove('active');
+    navbar.classList.remove('active');
 
 }
 
-let user = document.querySelector('.cart');
+let user = document.querySelector('.user');
 
 document.querySelector('#user-icon').onclick = () => {
     user.classList.toggle('active');
     search.classList.remove('active');
-    cart.classList.toggle('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    user.classList.remove('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    user.classList.remove('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
 }
 
 
@@ -31,8 +52,6 @@ let header = document.querySelector('header');
 window.addEventListener('scroll' , () =>{
   header.classList.toggle('shadow' , window.scrollY > 0);
 });
-
-
 
 
 var swiper = new Swiper('.new-arrival', {
